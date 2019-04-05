8acb6038c997032196bc75642803164b133aba53
ckie <git-525ff67@ckie.dev>
2019-04-05 14:29:50 +0300

    Add better logging and tell Rethink what's up

--- app.js
+++ app.js
@@ -6,27 +6,66 @@ const GHWebhook = require("express-github-webhook");
 const webhookHandler = GHWebhook({ path: "/webhook", secret });
 const fs = require("mz/fs");
 const GitUrlParse = require("git-url-parse");
-const os = require("os");
+require("log-node")();
+const log = require("log"); 
 const chdproc = require("mz/child_process");
-// const github = require("@octokit/rest");
+const dbOpts = {
+    db: process.env.db || `sdab`,
+};
+if (process.env.DB_ADDR) dbOpts.servers = [{ host: process.env.DB_ADDR.split(":")[0], port: parseInt(process.env.DB_ADDR.split(":")[1], 10) }]
+const origConsoleLog = console.log;
+console.log = log;
+const r = require(`rethinkdbdash`)(dbOpts); // Connect to RethinkDB
+console.log = origConsoleLog;
+
 // Create repos folder and catch "already exists" error incase it is thrown.
 fs.mkdir("repos")
     .catch(error => {
         if (error.message.includes("EEXIST")) return;
         else throw error;
     });
-
+async function createDBStruct() {
+    r.tableCreate("builds");
+}
+createDBStruct().catch(() => {});
 app.use(express.json());
 app.use(webhookHandler);
 
 webhookHandler.on("push", async (___USELESS___, data) => {
-    // console.log(data);
-    // const head_sha = data.after;
+    try {
+        await handlePush(data);
+    } catch (error) {
+        log.error(error);
+        await r.table("builds").update({
+            id: head_sha,
+            status: "fail",
+            statusReason: error.message,
+            updatedAt: Date.now()
+        });
+    }
+});
+
+async function handlePush(data) {
+    if (process.env.DEBUG_PRINT_REQ === "TRUE") console.log(data);
+    const head_sha = data.after;
     const author = data.head_commit.author.name;
     const cloneURL = data.repository.ssh_url;
-    const repoUniqueHuman = data.repository.id+data.repository.full_name.replace("/", "-");
-    const repoUniqueHumanAbs = process.cwd()+"/repos/"+data.repository.id+data.repository.full_name.replace("/", "-");
+    const repoUniqueHuman = data.repository.id + data.repository.full_name.replace("/", "-");
+    const repoUniqueHumanAbs = process.cwd() + "/repos/" + data.repository.id + data.repository.full_name.replace("/", "-");
     const gbranch = data.ref.split("/")[2];
+
+    await r.table("builds").insert({
+        id: head_sha,
+        status: "processing",
+        createdAt: Date.now(),
+        repo: {
+            id: data.repository.id,
+            name: data.repository.name,
+            author,
+            authorID: data.repository.owner.id
+        }
+    });
+
     // Cloning the repo
     const { source } = GitUrlParse(cloneURL); // example value: "github.com"
     // await chdproc.exec(`mkdir ~/.ssh; ssh-keyscan -t rsa ${source} >> ~/.ssh/known_hosts`);
@@ -34,18 +73,22 @@ webhookHandler.on("push", async (___USELESS___, data) => {
         await chdproc.exec(`git clone --branch ${gbranch} ${cloneURL} repos/${repoUniqueHuman}`);
     } catch (error) {
         if (error.message.includes("already exists and is not an empty directory.")) {
-            await chdproc.exec(`git pull`, {cwd: repoUniqueHumanAbs});
+            await chdproc.exec(`git pull`, { cwd: repoUniqueHumanAbs });
         }
         else throw error;
     }
-    if (!(await fs.exists(`repos/${repoUniqueHuman}/Dockerfile`))) return console.log(`${Date()}: Dockerfile missing for ${data.repository.full_name}!`);
-    if (!(await fs.exists(`repos/${repoUniqueHuman}/sdab.json`))) return console.log(`${Date()}: sdab.json missing for ${data.repository.full_name}!`);
+    
+    if (!(await fs.exists(`repos/${repoUniqueHuman}/Dockerfile`))) throw new Error(`Dockerfile missing for ${data.repository.full_name}!`);
+    if (!(await fs.exists(`repos/${repoUniqueHuman}/sdab.json`))) throw new Error(`sdab.json missing for ${data.repository.full_name}!`);
+    
     delete require.cache[`${repoUniqueHumanAbs}/sdab.json`]; // Clear cache
     const cfg = require(`${repoUniqueHumanAbs}/sdab.json`);
     const tagArg = `${cfg.customRegistry ? `${cfg.customRegistry}/` : ""}${cfg.tag}`;
-    if (typeof cfg.tag !== "string") return console.log(`${data.repository.full_name} sdab.json: tag is not a string or is missing`);
+
+    if (typeof cfg.tag !== "string") throw new Error(`${data.repository.full_name} sdab.json: tag is not a string or is missing`);
+    
     await chdproc.exec(`docker build -t ${tagArg} repos/${repoUniqueHuman}`);
-    console.log(`${Date()}: ${data.repository.full_name} event: built docker image ${cfg.tag}`);
+    log(`${data.repository.full_name} event: built docker image ${cfg.tag}`);
     try {
         await chdproc.exec(cfg.customRegistry ? `docker login ${cfg.customRegistry}` : `docker login`);
     } catch (derr) {
@@ -54,9 +97,14 @@ webhookHandler.on("push", async (___USELESS___, data) => {
             throw derr;
         } else throw derr;
     }
-    console.log(`${Date()}: ${data.repository.full_name} event: pushing docker image ${tagArg}`);
-    console.log(await chdproc.exec(`docker push ${tagArg}`));
-    console.log(`${Date()}: ${data.repository.full_name} event: pushed docker image ${tagArg}`);
-});
+    log(`${data.repository.full_name} event: pushing docker image ${tagArg}`);
+    log(await chdproc.exec(`docker push ${tagArg}`));
+    log(`${data.repository.full_name} event: pushed docker image ${tagArg}`);
+    await r.table("builds").update({
+        id: head_sha,
+        status: "ok",
+        updatedAt: Date.now()
+    });
+}
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => log(`Listening on port ${port}`));
