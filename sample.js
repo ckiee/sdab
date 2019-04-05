b8b8b62e3d94460fe915742f00d593b9b4be8d04
RONTheCookie <ronthecookie0101@gmail.com>
2019-04-05 14:29:50 +0300

    Add better logging and tell Rethink what's up

---
+++ sample.js
@@ -0,0 +1,194 @@
+sample = { ref: 'refs/heads/master',
+  before: '16ae94a203db70fd4aa1e710f11c9aca1b4b77a4',
+  after: '77527b4f37c46cc352d173a71053392d4bff0dd8',
+  created: false,
+  deleted: false,
+  forced: true,
+  base_ref: null,
+  compare:
+   'https: //github.com/RONTheCookie/ronthecookie.me/compare/16ae94a203db...77527b4f37c4',
+  commits: [],
+  head_commit: { id: '77527b4f37c46cc352d173a71053392d4bff0dd8',
+     tree_id: '5d8dc2b5e53c312fdf9d6b6073a1c1680a9958a9',
+     distinct: true,
+     message: 'Open in new tab links ul',
+     timestamp: '2019-04-05T09: 58: 38+03: 00',
+     url:
+      'https: //github.com/RONTheCookie/ronthecookie.me/commit/77527b4f37c46cc352d173a71053392d4bff0dd8',
+     author: { name: 'RONTheCookie',
+        email: 'ronthecookie0101@gmail.com',
+        username: 'RONTheCookie'
+    },
+     committer: { name: 'RONTheCookie',
+        email: 'ronthecookie0101@gmail.com',
+        username: 'RONTheCookie'
+    },
+     added: [],
+     removed: [],
+     modified: [ 'static/index.html'
+    ]
+},
+  repository: { id: 122309396,
+     node_id: 'MDEwOlJlcG9zaXRvcnkxMjIzMDkzOTY=',
+     name: 'ronthecookie.me',
+     full_name: 'RONTheCookie/ronthecookie.me',
+     private: false,
+     owner: { name: 'RONTheCookie',
+        email: 'RONTheCookie@users.noreply.github.com',
+        login: 'RONTheCookie',
+        id:'' ,
+        node_id: '',
+        avatar_url: 'https: //avatars1.githubusercontent.com/u/25263210?v=4',
+        gravatar_id: '',
+        url: 'https: //api.github.com/users/RONTheCookie',
+        html_url: 'https: //github.com/RONTheCookie',
+        followers_url: 'https: //api.github.com/users/RONTheCookie/followers',
+        following_url:
+         'https: //api.github.com/users/RONTheCookie/following{/other_user}',
+        gists_url: 'https: //api.github.com/users/RONTheCookie/gists{/gist_id}',
+        starred_url:
+         'https: //api.github.com/users/RONTheCookie/starred{/owner}{/repo}',
+        subscriptions_url: 'https: //api.github.com/users/RONTheCookie/subscriptions',
+        organizations_url: 'https: //api.github.com/users/RONTheCookie/orgs',
+        repos_url: 'https: //api.github.com/users/RONTheCookie/repos',
+        events_url: 'https: //api.github.com/users/RONTheCookie/events{/privacy}',
+        received_events_url: 'https: //api.github.com/users/RONTheCookie/received_events',
+        type: 'User',
+        site_admin: false
+    },
+     html_url: 'https: //github.com/RONTheCookie/ronthecookie.me',
+     description: "My website, that's it.",
+     fork: false,
+     url: 'https: //github.com/RONTheCookie/ronthecookie.me',
+     forks_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/forks',
+     keys_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/keys{/key_id}',
+     collaborators_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/collaborators{/collaborator}',
+     teams_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/teams',
+     hooks_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/hooks',
+     issue_events_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/issues/events{/number}',
+     events_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/events',
+     assignees_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/assignees{/user}',
+     branches_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/branches{/branch}',
+     tags_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/tags',
+     blobs_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/git/blobs{/sha}',
+     git_tags_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/git/tags{/sha}',
+     git_refs_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/git/refs{/sha}',
+     trees_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/git/trees{/sha}',
+     statuses_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/statuses/{sha}',
+     languages_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/languages',
+     stargazers_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/stargazers',
+     contributors_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/contributors',
+     subscribers_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/subscribers',
+     subscription_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/subscription',
+     commits_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/commits{/sha}',
+     git_commits_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/git/commits{/sha}',
+     comments_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/comments{/number}',
+     issue_comment_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/issues/comments{/number}',
+     contents_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/contents/{+path}',
+     compare_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/compare/{base}...{head}',
+     merges_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/merges',
+     archive_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/{archive_format}{/ref}',
+     downloads_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/downloads',
+     issues_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/issues{/number}',
+     pulls_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/pulls{/number}',
+     milestones_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/milestones{/number}',
+     notifications_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/notifications{?since,all,participating}',
+     labels_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/labels{/name}',
+     releases_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/releases{/id}',
+     deployments_url:
+      'https: //api.github.com/repos/RONTheCookie/ronthecookie.me/deployments',
+     created_at: 1519201277,
+     updated_at: '2019-04-05T07: 07: 23Z',
+     pushed_at: 1554457752,
+     git_url: 'git: //github.com/RONTheCookie/ronthecookie.me.git',
+     ssh_url: 'git@github.com:RONTheCookie/ronthecookie.me.git',
+     clone_url: 'https: //github.com/RONTheCookie/ronthecookie.me.git',
+     svn_url: 'https: //github.com/RONTheCookie/ronthecookie.me',
+     homepage: 'https: //ronthecookie.me',
+     size: 53,
+     stargazers_count: 0,
+     watchers_count: 0,
+     language: 'CSS',
+     has_issues: true,
+     has_projects: true,
+     has_downloads: true,
+     has_wiki: true,
+     has_pages: false,
+     forks_count: 0,
+     mirror_url: null,
+     archived: false,
+     disabled: false,
+     open_issues_count: 0,
+     license: { key: 'mit',
+        name: 'MIT License',
+        spdx_id: 'MIT',
+        url: 'https: //api.github.com/licenses/mit',
+        node_id: ''
+    },
+     forks: 0,
+     open_issues: 0,
+     watchers: 0,
+     default_branch: 'master',
+     stargazers: 0,
+     master_branch: 'master'
+},
+  pusher: { name: 'RONTheCookie',
+     email: 'RONTheCookie@users.noreply.github.com'
+},
+  sender: { login: 'RONTheCookie',
+     id: '',
+     node_id: '',
+     avatar_url: 'https: //avatars1.githubusercontent.com/u/25263210?v=4',
+     gravatar_id: '',
+     url: 'https: //api.github.com/users/RONTheCookie',
+     html_url: 'https: //github.com/RONTheCookie',
+     followers_url: 'https: //api.github.com/users/RONTheCookie/followers',
+     following_url:
+      'https: //api.github.com/users/RONTheCookie/following{/other_user}',
+     gists_url: 'https: //api.github.com/users/RONTheCookie/gists{/gist_id}',
+     starred_url:
+      'https: //api.github.com/users/RONTheCookie/starred{/owner}{/repo}',
+     subscriptions_url: 'https: //api.github.com/users/RONTheCookie/subscriptions',
+     organizations_url: 'https: //api.github.com/users/RONTheCookie/orgs',
+     repos_url: 'https: //api.github.com/users/RONTheCookie/repos',
+     events_url: 'https: //api.github.com/users/RONTheCookie/events{/privacy}',
+     received_events_url: 'https: //api.github.com/users/RONTheCookie/received_events',
+     type: 'User',
+     site_admin: false
+}
+}
