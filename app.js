ae7b9da4634dfc014f16057a3cbd995042312d78
RONTheCookie <me@ronthecookie.me>
2019-05-04 11:51:46 +0300

    Code style & more verbose logs

--- app.js
+++ app.js
@@ -7,23 +7,28 @@ const webhookHandler = GHWebhook({ path: "/webhook", secret });
 const fs = require("mz/fs");
 const GitUrlParse = require("git-url-parse");
 require("log-node")();
-const log = require("log"); 
+const log = require("log");
 const chdproc = require("mz/child_process");
 const dbOpts = {
-    db: process.env.db || `sdab`,
+    db: process.env.db || `sdab`
 };
-if (process.env.DB_ADDR) dbOpts.servers = [{ host: process.env.DB_ADDR.split(":")[0], port: parseInt(process.env.DB_ADDR.split(":")[1], 10) }]
+if (process.env.DB_ADDR)
+    dbOpts.servers = [
+        {
+            host: process.env.DB_ADDR.split(":")[0],
+            port: parseInt(process.env.DB_ADDR.split(":")[1], 10)
+        }
+    ];
 const origConsoleLog = console.log;
 console.log = log;
 const r = require(`rethinkdbdash`)(dbOpts); // Connect to RethinkDB
 console.log = origConsoleLog;
 
 // Create repos folder and catch "already exists" error incase it is thrown.
-fs.mkdir("repos")
-    .catch(error => {
-        if (error.message.includes("EEXIST")) return;
-        else throw error;
-    });
+fs.mkdir("repos").catch((error) => {
+    if (error.message.includes("EEXIST")) return;
+    else throw error;
+});
 async function createDBStruct() {
     r.tableCreate("builds");
 }
@@ -51,8 +56,13 @@ async function handlePush(data) {
     const author = data.head_commit.author.name;
     const commitMsg = data.head_commit.message;
     const cloneURL = data.repository.ssh_url;
-    const repoUniqueHuman = data.repository.id + data.repository.full_name.replace("/", "-");
-    const repoUniqueHumanAbs = process.cwd() + "/repos/" + data.repository.id + data.repository.full_name.replace("/", "-");
+    const repoUniqueHuman =
+        data.repository.id + data.repository.full_name.replace("/", "-");
+    const repoUniqueHumanAbs =
+        process.cwd() +
+        "/repos/" +
+        data.repository.id +
+        data.repository.full_name.replace("/", "-");
     const gbranch = data.ref.split("/")[2];
 
     await r.table("builds").insert({
@@ -74,30 +84,54 @@ async function handlePush(data) {
     const { source } = GitUrlParse(cloneURL); // example value: "github.com"
     // await chdproc.exec(`mkdir ~/.ssh; ssh-keyscan -t rsa ${source} >> ~/.ssh/known_hosts`);
     try {
-        await chdproc.exec(`git clone --branch ${gbranch} ${cloneURL} repos/${repoUniqueHuman}`);
+        await chdproc.exec(
+            `git clone --branch ${gbranch} ${cloneURL} repos/${repoUniqueHuman}`
+        );
     } catch (error) {
-        if (error.message.includes("already exists and is not an empty directory.")) {
+        if (
+            error.message.includes(
+                "already exists and is not an empty directory."
+            )
+        ) {
             await chdproc.exec(`git pull`, { cwd: repoUniqueHumanAbs });
-        }
-        else throw error;
+        } else throw error;
     }
-    
-    if (!(await fs.exists(`repos/${repoUniqueHuman}/Dockerfile`))) throw new Error(`Dockerfile missing for ${data.repository.full_name}!`);
-    if (!(await fs.exists(`repos/${repoUniqueHuman}/sdab.json`))) throw new Error(`sdab.json missing for ${data.repository.full_name}!`);
-    
+
+    if (!(await fs.exists(`repos/${repoUniqueHuman}/Dockerfile`)))
+        throw new Error(`Dockerfile missing for ${data.repository.full_name}!`);
+    if (!(await fs.exists(`repos/${repoUniqueHuman}/sdab.json`)))
+        throw new Error(`sdab.json missing for ${data.repository.full_name}!`);
+
     delete require.cache[`${repoUniqueHumanAbs}/sdab.json`]; // Clear cache
     const cfg = require(`${repoUniqueHumanAbs}/sdab.json`);
-    const tagArg = `${cfg.customRegistry ? `${cfg.customRegistry}/` : ""}${cfg.tag}`;
+    const tagArg = `${cfg.customRegistry ? `${cfg.customRegistry}/` : ""}${
+        cfg.tag
+    }`;
 
-    if (typeof cfg.tag !== "string") throw new Error(`${data.repository.full_name} sdab.json: tag is not a string or is missing`);
-    
-    await chdproc.exec(`docker build -t ${tagArg} repos/${repoUniqueHuman}`);
+    if (typeof cfg.tag !== "string")
+        throw new Error(
+            `${
+                data.repository.full_name
+            } sdab.json: tag is not a string or is missing`
+        );
+    log(
+        await chdproc.exec(`docker build -t ${tagArg} repos/${repoUniqueHuman}`)
+    );
+    log(`${data.repository.full_name} event: building docker image ${cfg.tag}`);
     log(`${data.repository.full_name} event: built docker image ${cfg.tag}`);
     try {
-        await chdproc.exec(cfg.customRegistry ? `docker login ${cfg.customRegistry}` : `docker login`);
+        await chdproc.exec(
+            cfg.customRegistry
+                ? `docker login ${cfg.customRegistry}`
+                : `docker login`
+        );
     } catch (derr) {
         if (derr.message.includes("Cannot perform an interactive login")) {
-            console.log(`${Date()}: ${data.repository.full_name} ERROR: You have to login on the host first before starting a build.`);
+            console.log(
+                `${Date()}: ${
+                    data.repository.full_name
+                } ERROR: You have to login on the host first before starting a build.`
+            );
             throw derr;
         } else throw derr;
     }
